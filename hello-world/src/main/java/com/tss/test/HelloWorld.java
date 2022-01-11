4f1df8fe7b7f0c925ee75221cc2c8a9d1a4d190f
vinay <vinay@192.168.1.7>
2022-01-11 11:04:01 +0530

    small changes.

--- hello-world/src/main/java/com/tss/test/HelloWorld.java
+++ hello-world/src/main/java/com/tss/test/HelloWorld.java
@@ -12,7 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("hello world");
 	}
